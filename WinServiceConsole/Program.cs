c2cc7a28b9b3ab029829ed41fdca618a99c9a720
alexf-dev <regalex82@gmail.com>
2020-03-12 01:07:08 +0600

    First work code

--- WinServiceConsole/Program.cs
+++ WinServiceConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,23 @@ namespace WinServiceConsole
     {
         static void Main(string[] args)
         {
+            Task.Factory.StartNew(() =>
+            {                
+                while (true)
+                {
+                    var server = new NamedPipeServerStream("WinServiceConsole");
+                    server.WaitForConnection();
+                    StreamReader reader = new StreamReader(server);
+                    StreamWriter writer = new StreamWriter(server);
+                    var line = reader.ReadLine();
+                    Console.WriteLine(line);
+                    writer.Flush();
+                    server.Disconnect();
+                    server.Close();
+                }
+            });
+
+            Console.ReadLine();
         }
     }
 }
